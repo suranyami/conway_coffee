76439aafac92f0c60c6ff044e80f3c40844d69d8
David Parry <david.parry@suranyami.com>
2011-08-24 20:28:56 +1000

    Added neighbour_count method.

--- scripts/conway.coffee
+++ scripts/conway.coffee
@@ -1,24 +1,47 @@
 class Game
   constructor: ->
-    @x = 100
-    @y = 100
-    @board = []
+    @x = 10
+    @y = 10
+    @board = new Array(@x)
   
+  random_cell: ->
+    Math.floor(Math.random() * 2)
+
   init_board: ->
-    for x in [0..@x]
-      for y in [0..@y]
-        console.log "#{x} #{y}"
-        @board[x][y] = Math.floor(Math.rand * 2)
+    for x in [0..@x - 1]
+      @board[x] = new Array(@y)
+      for y in [0..@y - 1]
+        @board[x][y] = this.random_cell()
+    console.log(@board)
+
+  neighbour_count: (x, y) ->
+    result = 0
+    for i in [-1, 0, 1]
+      cur_x = x + i
+      cur_x = 0 if cur_x >= @x
+      cur_x = @x - 1 if cur_x <= 0
+      for j in [-1, 0, 1]
+        cur_y = y + i
+        cur_y = 0 if cur_y >= @y
+        cur_y = @y - 1 if cur_y <= 0
+        result += @board[cur_x][cur_y] unless i = 0 and j = 0
 
   print_board: ->
     result = ""
-    for x in [0..@x]
-      for y in [0..@y]
-        result += @board[x][y] + "X"
-      result += "<br/>"
-    result
-
+    for x in [0..@x - 1]
+      result += "<tr>"
+      for y in [0..@y - 1]
+        count = this.neighbour_count(x, y)
+        result += "<td>#{count}</td>"
+      result += "</tr>"
+    "<table>#{result}</table>"
+    
+  live_or_die: (x, y) ->
+    neighbour_count(x, y)
+              
 $(document).ready ->
   game = new Game
-  game.init_board
-  $("#banana").html(game.print_board)
+  game.init_board()
+  
+  
+  $("#banana").html(game.print_board())
--- scripts/conway.js
+++ scripts/conway.js
@@ -2,20 +2,51 @@
   var Game;
   Game = (function() {
     function Game() {
-      this.x = 100;
-      this.y = 100;
-      this.board = [];
+      this.x = 10;
+      this.y = 10;
+      this.board = new Array(this.x);
     }
+    Game.prototype.random_cell = function() {
+      return Math.floor(Math.random() * 2);
+    };
     Game.prototype.init_board = function() {
-      var x, y, _ref, _results;
+      var x, y, _ref, _ref2;
+      for (x = 0, _ref = this.x - 1; 0 <= _ref ? x <= _ref : x >= _ref; 0 <= _ref ? x++ : x--) {
+        this.board[x] = new Array(this.y);
+        for (y = 0, _ref2 = this.y - 1; 0 <= _ref2 ? y <= _ref2 : y >= _ref2; 0 <= _ref2 ? y++ : y--) {
+          this.board[x][y] = this.random_cell();
+        }
+      }
+      return console.log(this.board);
+    };
+    Game.prototype.neighbour_count = function(x, y) {
+      var cur_x, cur_y, i, j, result, _i, _len, _ref, _results;
+      result = 0;
+      _ref = [-1, 0, 1];
       _results = [];
-      for (x = 0, _ref = this.x; 0 <= _ref ? x <= _ref : x >= _ref; 0 <= _ref ? x++ : x--) {
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        i = _ref[_i];
+        cur_x = x + i;
+        if (cur_x >= this.x) {
+          cur_x = 0;
+        }
+        if (cur_x <= 0) {
+          cur_x = this.x - 1;
+        }
         _results.push((function() {
-          var _ref2, _results2;
+          var _j, _len2, _ref2, _results2;
+          _ref2 = [-1, 0, 1];
           _results2 = [];
-          for (y = 0, _ref2 = this.y; 0 <= _ref2 ? y <= _ref2 : y >= _ref2; 0 <= _ref2 ? y++ : y--) {
-            console.log("" + x + " " + y);
-            _results2.push(this.board[x][y] = Math.floor(Math.rand * 2));
+          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+            j = _ref2[_j];
+            cur_y = y + i;
+            if (cur_y >= this.y) {
+              cur_y = 0;
+            }
+            if (cur_y <= 0) {
+              cur_y = this.y - 1;
+            }
+            _results2.push(!(i = 0 && (j = 0)) ? result += this.board[cur_x][cur_y] : void 0);
           }
           return _results2;
         }).call(this));
@@ -23,22 +54,27 @@
       return _results;
     };
     Game.prototype.print_board = function() {
-      var result, x, y, _ref, _ref2;
+      var count, result, x, y, _ref, _ref2;
       result = "";
-      for (x = 0, _ref = this.x; 0 <= _ref ? x <= _ref : x >= _ref; 0 <= _ref ? x++ : x--) {
-        for (y = 0, _ref2 = this.y; 0 <= _ref2 ? y <= _ref2 : y >= _ref2; 0 <= _ref2 ? y++ : y--) {
-          result += this.board[x][y] + "X";
+      for (x = 0, _ref = this.x - 1; 0 <= _ref ? x <= _ref : x >= _ref; 0 <= _ref ? x++ : x--) {
+        result += "<tr>";
+        for (y = 0, _ref2 = this.y - 1; 0 <= _ref2 ? y <= _ref2 : y >= _ref2; 0 <= _ref2 ? y++ : y--) {
+          count = this.neighbour_count(x, y);
+          result += "<td>" + count + "</td>";
         }
-        result += "<br/>";
+        result += "</tr>";
       }
-      return result;
+      return "<table>" + result + "</table>";
+    };
+    Game.prototype.live_or_die = function(x, y) {
+      return neighbour_count(x, y);
     };
     return Game;
   })();
   $(document).ready(function() {
     var game;
     game = new Game;
-    game.init_board;
-    return $("#banana").html(game.print_board);
+    game.init_board();
+    return $("#banana").html(game.print_board());
   });
 }).call(this);
